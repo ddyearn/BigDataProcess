fc10272c5a4a88f56d5e503a278b93f5b6b0f4a2
seungyeon <bestsy777@gmail.com>
2022-11-01 23:44:13 +0900

    uberr

--- HW3/UBERStudent20200937.py
+++ HW3/UBERStudent20200937.py
@@ -1,39 +1,38 @@
 #!/usr/bin/python3
-import sys
-import calendar
+import sys, calendar
+
+uberList = []
+dayOfWeek = {0:'MON', 1:'TUE', 2:'WED', 3:'THU', 4:'FRI', 5:'SAT', 6:'SUN'}
 
 with open(sys.argv[1], "rt") as f:
-	data = f.read()
-
-ubers = []
-days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-
-rows = data.split("\n")
-for row in rows:
-	fields = row.split(",")
-	
-	base = fields[0]
-	
-	dt = fields[1].split("/")
-	day = days[calendar.weekday(int(dt[2]), int(dt[0]), int(dt[1]))]
-	
-	vehicle = int(fields[2])
-	trip = int(fields[3])
-	
-	flag = 0
-	
-	for uber in ubers:
-		if uber[0] == base and uber[1] == day:
-			uber[2] += vehicle
-			uber[3] += trip
-			flag = 1
-			break
-			
-	if flag == 0:
-		ubers.append([base, day, vehicle, trip])
-
-with open(sys.argv[2], "wt") as fp:
-	for uber in ubers:
-		fp.write('%s,%s %d,%d\n' % (uber[0], uber[1], uber[2], uber[3]))
+	while True:
+		row = f.readline()
+		if not row: break
 		
+		fields = row.split(",")
+	
+		base = fields[0]
+		
+		dt = fields[1].split("/")
+		day = days[calendar.weekday(int(dt[0]), int(dt[1]), int(dt[2]))]
+	
+		vehicle = int(fields[2])
+		trip = int(fields[3])
+	
+		flag = 0
+		for uber in uberList:
+			if uber[0] == base and uber[1] == day:
+				uber[2] += vehicle
+				uber[3] += trip
+				flag = 1
+				break
+			
+		if flag == 0:
+			uberList.append([base, day, vehicle, trip])
+			
+		fp = open(sys.argv[2], "wt")
+		for uber in uberList:
+			fp.write("%s,%s %d,%d\n" %(uber[0], uber[1], uber[2], uber[3]))
+		fp.close()
+
 
